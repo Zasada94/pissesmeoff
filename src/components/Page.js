2d8d8c40f2ce48cf7713fbf36347669fa2d1c2b9
Zasada94 <pawelzasada2@wp.pl>
2022-09-30 15:20:07 +0200

    first posts added

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Container from "@mui/material/Container";
 import PostList from "./PostList";
@@ -11,20 +11,55 @@ function Page() {
 	const regular = useSelector((state) => state.posts.regular);
 	const favourites = useSelector((state) =>
 		[...state.posts.regular, ...state.posts.hot].filter(
-			(post) => post.favourite
+			(post) => post.favourites
 		)
 	);
 	return (
 		<Container>
-				<Routes>
-					<Route path="/" element={<HomePage />}></Route>
-					<Route path="/pissesmeoff" element={<HomePage />}></Route>
-					<Route path="/regular" element={<PostList array={regular} />}></Route>
-					<Route path="/hot" element={<PostList array={hot} />}></Route>
-					<Route path="/favourites" element={<PostList />}></Route>
-					<Route path="/add_post" element={<AddPost />}></Route>
-					<Route path="*" element={<Navigate to="/" replace />}></Route>
-				</Routes>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<>
+							<HomePage />,<PostList array={regular} />
+						</>
+					}
+				></Route>
+				<Route
+					path="/pissesmeoff"
+					element={
+						<>
+							<HomePage />,<PostList array={regular} />
+						</>
+					}
+				></Route>
+				<Route
+					path="/regular"
+					element={
+						<>
+							<HomePage />,<PostList array={regular} />
+						</>
+					}
+				></Route>
+				<Route
+					path="/hot"
+					element={
+						<>
+							<HomePage />,<PostList array={hot} />
+						</>
+					}
+				></Route>
+				<Route
+					path="/favourites"
+					element={
+						<>
+							<HomePage />,<PostList array={favourites} />
+						</>
+					}
+				></Route>
+				<Route path="/add_post" element={<AddPost />}></Route>
+				<Route path="*" element={<Navigate to="/" replace />}></Route>
+			</Routes>
 		</Container>
 	);
 }
